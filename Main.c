d10457bae99a8a01fc202ff047505a5ffb642f27
SeyhanHAHAHAHAHAHAAH <doganseyhan098@gmail.com>
2024-03-16 00:31:43 +0100

    modified:   Main.c

--- Main.c
+++ Main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-// Function prototype for createMap()
+// Function prototyp für createMap()
 void createMap(SDL_Renderer *renderer);
 
 int main() {
-    // Initialisierung von SDL
+    // Initialisierung für SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL_Init fehlgeschlagen: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Erstellung des SDL-Fensters
+    // Erstellung SDL-Fenster
     SDL_Window* window = SDL_CreateWindow(
         "SDL Fenster",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        640, 640, // Breite und Höhe des Fensters
+        640, 640, // Breite und Höhe 
         SDL_WINDOW_SHOWN
     );
     if (window == NULL) {
@@ -27,11 +27,21 @@ int main() {
    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED| SDL_RENDERER_PRESENTVSYNC);
    if (renderer == NULL){
     printf("SDL_CreateRenderer fehlgeschlagen: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);//falls render fehlschlägt auch window schliessen
+    SDL_Quit();
     return 1;
+   }
+   SDL_Surface *surface = IMG_Load("resources/hello.png");
+   if (surface==NULL)
+   {
+    printf("SDL_Surface fehlgeschlagen: %s\n", SDL_GetError());
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit;
    }
     createMap(renderer);
 
-    // Schleife, die das Fenster geöffnet hält, bis der Benutzer es schließt
+    // Schleife die das fenster geöffnet hält bis benutzer es schließt
     SDL_Event event;
     int running = 1;
     while (running) {
@@ -42,17 +52,17 @@ int main() {
         }
     }
 
-    // Aufräumen und Beenden von SDL
+    //Beenden von SDL
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
 
-// Definition of createMap() function
+// Definition von createMap() function
 void createMap(SDL_Renderer *renderer){
 
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255); // Corrected function call
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255); 
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_Rect rect = {200, 200, 50, 50};
